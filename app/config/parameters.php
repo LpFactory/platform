03f8b96d0c0f4e640651a8b9ad486fe719e5d92a
Jonathan Bouzekri <jonathan.bouzekri@gmail.com>
2015-04-11 21:30:36 +0200

    docker compose

--- app/config/parameters.php
+++ app/config/parameters.php
@@ -4,9 +4,9 @@ $container->setParameter('database_driver', 'pdo_mysql');
 if (php_sapi_name() == 'cli') {
     $container->setParameter('database_host', '127.0.0.1');
 } else {
-    $container->setParameter('database_host', 'dev_nginx_mysql');
+    $container->setParameter('database_host', 'opsitebuilderdb');
 }
 $container->setParameter('database_port', '');
 $container->setParameter('database_name', 'opsite_builder');
 $container->setParameter('database_user', 'root');
-$container->setParameter('database_password', '');
+$container->setParameter('database_password', 'opsitebuilder');

9513ed0e2f9774235e4b5feec772c2f9785b1f7f
Jonathan Bouzekri <jonathan.bouzekri@gmail.com>
2015-04-14 22:59:03 +0200

    rework list with angular

--- src/OpSiteBuilder/Bundle/WebBundle/Resources/public/js/app.js
+++ src/OpSiteBuilder/Bundle/WebBundle/Resources/public/js/app.js
@@ -1,54 +1,45 @@
-(function(opsiteconf){
+(function(opsiteconf, angular){
     'use strict';
 
-    var BlockView = Backbone.View.extend({
-        tagName: "div",
-        className: "document-row",
-        events: {
-            "click .opsitebuilder-block-edit" : "editClick",
-            "click .opsitebuilder-block-edit-cancel": "editCancel",
-            "click .opsitebuilder-block-remove": "removeBlock"
-        },
-        render: function() {
-            this.$el.html(this.model.get('template'));
-            return this;
-        },
-        editClick: function() {
-            this.model.set('template', "<a class='opsitebuilder-block-edit-cancel'>Cancel</a> <strong>Editing</strong>");
-        },
-        editCancel: function() {
-            this.model.set('template', '<a class="opsitebuilder-block-edit" href="#edit-57">Edit</a><strong>Content for block 57</strong>');
-        },
-        removeBlock: function() {
-            this.remove();
-        },
-        initialize: function() {
-            this.listenTo(this.model, 'change', this.render);
-        }
+    var page = {
+        id: 1,
+        name: "page title toto",
+        blocks: [
+            {
+                id: 123,
+                type: "simple-block",
+                name: "block1",
+                template: 'Name: test {{ block.name }}'
+            },
+            {
+                id: 456,
+                type: "simple-block",
+                name: "block2",
+                template: 'Name: test {{ block.name }}'
+            }
+        ]
+    };
+
+    angular.module('OpSiteBuilderApp', [])
+        .controller('PageEditController', ['$scope', '$rootScope', '$compile', function ($scope, $rootScope, $compile) {
+            $scope.page = page;
+        }])
+        .directive('opsiteSimpleBlock', function($compile, $http) {
+            return {
+                compile: function(tElement, tAttrs) {
+                    // Use return function to access scope and have block object
+                    return function (scope, element, attrs) {
+                        $http
+                            .get('/block-'+scope.block.type+'.html')
+                            .success(function(html) {
+                                element.html($compile(html)(scope));
+                            });
+                    };
+                }
+            };
+        });
+
+    angular.element(document).ready(function() {
+        angular.bootstrap(document, ['OpSiteBuilderApp']);
     });
-
-    var blockList = Backbone.Collection.extend({
-    });
-
-    var blocks = new blockList(opsiteconf.blocks);
-
-    var AppView = Backbone.View.extend({
-        el: $('#test'),
-        render: function() {
-            this.$el.empty();
-
-            var container = document.createDocumentFragment();
-            blocks.each(function(block) {
-                var blockView = new BlockView({ model: block });
-                container.appendChild(blockView.render().el);
-            }, this);
-
-            this.$el.append(container);
-        }
-    });
-
-    var App = new AppView;
-    App.render();
-
-    opsiteconf.toto = {test:"test"};
-})(window.opsiteconf);
+})(window.opsiteconf, angular);
